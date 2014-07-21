4d1057d9e3b388f087db2cd31015157a0d527089
alexkernphysiker <alex_kernphysiker@privatdemail.net>
2014-07-21 11:20:37 +0200

    sigma.h optimize

--- sigma.h
+++ sigma.h
@@ -6,21 +6,26 @@ template<typename numt>
 class Sigma{
 private:
 	std::list<numt> m_list;
+	numt m_avr;bool cached=false;
 public:
-	Sigma(){}
+	Sigma(){cached=false;}
 	virtual ~Sigma(){}
-	void AddValue(numt value){m_list.push_back(value);}
+	void AddValue(numt value){m_list.push_back(value);cached=false;}
 	numt getAverage(){
-		numt res=0;
-		foreach(double value,m_list)
-			res+=value;
-		return res/m_list.size();
+		if(!cached){
+			numt res=0;
+			foreach(double value,m_list)
+				res+=value;
+			m_avr=res/m_list.size();
+			cached=true;
+		}
+		return m_avr;
 	}
 	numt getSigma(){
-		numt avr=getAverage();
+		getAverage();
 		numt res=0;
 		foreach(double value,m_list)
-			res+=pow(value-avr,2);
+			res+=pow(value-m_avr,2);
 		res/=m_list.size()-1;
 		return sqrt(res);
 	}
