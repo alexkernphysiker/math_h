6c953df25a7d0d09c8fb9f3d1793261b7784a4f1
alexkernphysiker <alex_kernphysiker@privatdemail.net>
2014-08-17 17:06:30 +0200

    sigma optimization

--- sigma.h
+++ sigma.h
@@ -5,30 +5,25 @@
 template<typename numt>
 class Sigma{
 private:
-	std::list<numt> m_list;
-	numt m_avr;bool cached=false;
+	std::list<numt> m_list;numt m_sum;
+	bool cache; numt m_sigsqr;
 public:
-	Sigma(){cached=false;}
+	Sigma(){m_sum=0;cache=false;m_sigsqr=0;}
 	virtual ~Sigma(){}
-	void AddValue(numt value){m_list.push_back(value);cached=false;}
-	numt getAverage(){
-		if(!cached){
-			numt res=0;
-			foreach(double value,m_list)
-				res+=value;
-			m_avr=res/m_list.size();
-			cached=true;
-		}
-		return m_avr;
-	}
+	void AddValue(numt value){m_list.push_back(value);m_sum+=value;cache=false;}
+	numt getAverage(){return m_sum/m_list.size();}
 	int count(){return m_list.size();}
-	numt getSigma(){
-		getAverage();
-		numt res=0;
-		foreach(double value,m_list)
-			res+=pow(value-m_avr,2);
-		res/=m_list.size()-1;
-		return sqrt(res);
+	numt getSigmaSqr(){
+		if(!cache){
+			double average=getAverage();
+			m_sigsqr=0;
+			foreach(double value,m_list)
+				m_sigsqr+=pow(value-average,2);
+			m_sigsqr/=m_list.size()-1;
+			cache=true;
+		}
+		return m_sigsqr;
 	}
+	numt getSigma(){return sqrt(getSigmaSqr());}
 };
 #endif
