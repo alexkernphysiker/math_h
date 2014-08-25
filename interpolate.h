6425198e07c54db4d58e0d5386a2c0bed88f03c7
alexkernphysiker <alex_kernphysiker@privatdemail.net>
2014-08-25 12:27:13 +0200

    functable

--- interpolate.h
+++ interpolate.h
@@ -43,4 +43,43 @@ numt  interpolateLinear(indexer X, indexer2 Y, numt x){
 	int i=insertIndex(X,x);if(i<=0)throw;if(i>=X.count())throw;
 	numt k=(x-X[i-1])/(X[i]-X[i-1]);return Y[i-1]+k*(Y[i]-Y[i-1]);
 }
+
+template<class numt>
+class FuncTable{
+private:
+	numt* X;
+	numt* Y;
+	int cnt;
+public:
+	FuncTable(){cnt=2;X=new numt[cnt];Y=new numt[cnt];}
+	FuncTable(int sz){
+		if(sz<=1)throw;
+		cnt=sz;
+		X=new numt[cnt];
+		Y=new numt[cnt];
+	}
+	FuncTable(FuncTable &f){
+		cnt=f.cnt;
+		X=new numt[cnt];
+		Y=new numt[cnt];
+		for(int i=0; i<cnt;i++){
+			X[i]=(f.X[i]);
+			Y[i]=(f.Y[i]);
+		}
+	}
+	virtual ~FuncTable(){delete[] X; delete[] Y;}
+	void set(int i,numt x, numt y){X[i]=(x);Y[i]=(y);}
+	int size(){return cnt;}
+	numt operator()(numt x){
+		return Interpolate_Linear(0,cnt-1,X,Y,x);
+	}
+};
+template<class numt, class func>
+void fillFuncTable(FuncTable<numt> &tbl,numt from, numt to,func y){
+	numt step=(to-from)/(tbl.size()-1);
+	for(int i=0;i<tbl.size();i++){
+		numt x=from+(step*numt(i));
+		tbl.set(i,x,y(x));
+	}
+}
 #endif
