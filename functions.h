2ae2731eefe33bba5f2a51b66be4d6535d868dee
alexkernphysiker <alex_kernphysiker@privatdemail.net>
2014-10-05 01:47:24 +0200

    wrap2.h

--- functions.h
+++ functions.h
@@ -20,28 +20,4 @@ numt Polynom(numt x,indexer  p,unsigned int P, int index_offset=0){
 }
 template<unsigned int P,class numt, class indexer, int index_offset=0>
 numt Polynom(numt x,indexer  p){return Polynom<numt,indexer>(x,p,P,index_offset);}
-namespace FuncWrappers2{
-	template<class numt,class indexer,int x_ind>
-	numt arg(indexer X,indexer){return X[x_ind];}
-	template<class numt,class indexer,int p_ind>
-	numt par(indexer,indexer P){return P[p_ind];}
-	template<class numt,class indexer,numt (f)(numt),numt(F)(indexer,indexer)>
-	numt func(indexer X,indexer P){return f(F(X,P));}
-	template<class numt,class indexer,numt (f)(numt,numt),numt(F1)(indexer,indexer),numt(F2)(indexer,indexer)>
-	numt func2(indexer X,indexer P){return f(F1(X,P),F2(X,P));}
-	template<class numt,class indexer,numt (f)(numt,numt,numt),numt(F1)(indexer,indexer),numt(F2)(indexer,indexer),numt(F3)(indexer,indexer)>
-	numt func3(indexer X,indexer P){return f(F1(X,P),F2(X,P),F3(X,P));}
-	template<class numt,class indexer,numt(F1)(indexer,indexer),numt(F2)(indexer,indexer)>
-	numt add(indexer X,indexer P){return F1(X,P)+F2(X,P);}
-	template<class numt,class indexer,numt(F1)(indexer,indexer),numt(F2)(indexer,indexer)>
-	numt sub(indexer X,indexer P){return F1(X,P)-F2(X,P);}
-	template<class numt,class indexer,numt(F1)(indexer,indexer),numt(F2)(indexer,indexer)>
-	numt mul(indexer X,indexer P){return F1(X,P)*F2(X,P);}
-	template<class numt,class indexer,numt(F1)(indexer,indexer),numt(F2)(indexer,indexer)>
-	numt div(indexer X,indexer P){return F1(X,P)/F2(X,P);}
-	template<class numt,class indexer,numt(F1)(indexer,indexer),numt(F2)(indexer,indexer)>
-	numt power(indexer X,indexer P){return pow(F1(X,P),F2(X,P));}
-	template<class numt,class indexer,int x_ind, int par_ind, int p>
-	numt polynom(indexer X,indexer P){return Polynom(X[x_ind],P,p,par_ind);}
-}
 #endif // FUNCTIONS_H
---
+++ wrap2.h
@@ -0,0 +1,32 @@
+#ifdef use_num_type
+#ifdef use_indexer_type
+#ifndef WRAP2_H
+#define WRAP2_H
+#include "functions.h"
+namespace FuncWrappers2{
+	template<int x_ind>
+	use_num_type arg(use_indexer_type X,use_indexer_type){return X[x_ind];}
+	template<int p_ind>
+	use_num_type par(use_indexer_type,use_indexer_type P){return P[p_ind];}
+	template<use_num_type (f)(use_num_type),use_num_type(F)(use_indexer_type,use_indexer_type)>
+	use_num_type func(use_indexer_type X,use_indexer_type P){return f(F(X,P));}
+	template<use_num_type (f)(use_num_type,use_num_type),use_num_type(F1)(use_indexer_type,use_indexer_type),use_num_type(F2)(use_indexer_type,use_indexer_type)>
+	use_num_type func2(use_indexer_type X,use_indexer_type P){return f(F1(X,P),F2(X,P));}
+	template<use_num_type (f)(use_num_type,use_num_type,use_num_type),use_num_type(F1)(use_indexer_type,use_indexer_type),use_num_type(F2)(use_indexer_type,use_indexer_type),use_num_type(F3)(use_indexer_type,use_indexer_type)>
+	use_num_type func3(use_indexer_type X,use_indexer_type P){return f(F1(X,P),F2(X,P),F3(X,P));}
+	template<use_num_type(F1)(use_indexer_type,use_indexer_type),use_num_type(F2)(use_indexer_type,use_indexer_type)>
+	use_num_type add(use_indexer_type X,use_indexer_type P){return F1(X,P)+F2(X,P);}
+	template<use_num_type(F1)(use_indexer_type,use_indexer_type),use_num_type(F2)(use_indexer_type,use_indexer_type)>
+	use_num_type sub(use_indexer_type X,use_indexer_type P){return F1(X,P)-F2(X,P);}
+	template<use_num_type(F1)(use_indexer_type,use_indexer_type),use_num_type(F2)(use_indexer_type,use_indexer_type)>
+	use_num_type mul(use_indexer_type X,use_indexer_type P){return F1(X,P)*F2(X,P);}
+	template<use_num_type(F1)(use_indexer_type,use_indexer_type),use_num_type(F2)(use_indexer_type,use_indexer_type)>
+	use_num_type div(use_indexer_type X,use_indexer_type P){return F1(X,P)/F2(X,P);}
+	template<use_num_type(F1)(use_indexer_type,use_indexer_type),use_num_type(F2)(use_indexer_type,use_indexer_type)>
+	use_num_type power(use_indexer_type X,use_indexer_type P){return pow(F1(X,P),F2(X,P));}
+	template<int x_ind, int par_ind, int p>
+	use_num_type polynom(use_indexer_type X,use_indexer_type P){return Polynom(X[x_ind],P,p,par_ind);}
+}
+#endif // WRAP2_H
+#endif
+#endif
