71b390cafdd07ceba4de653aa2b7c0c875567761
alexkernphysiker <alex_kernphysiker@privatdemail.net>
2014-10-06 17:23:35 +0200

    wrap*.h upgrade

--- wrap1.h
+++ wrap1.h
@@ -2,8 +2,7 @@
 #ifdef use_num_type
 #ifdef use_indexer_type
 namespace FuncWrappers{
-	template<int i>
-	use_num_type par(use_indexer_type P){return P[i];}
+	template<int i>use_num_type par(use_indexer_type P){return P[i];}
 #define ___p_decl___ use_indexer_type
 #define ___p_decl2___ use_indexer_type P
 #define ___p_ P
