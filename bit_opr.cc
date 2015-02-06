5baedffdac23cf4881413ab907746a1c384f45ca
alexkernphysiker <kernphysiker@openmailbox.org>
2015-02-06 18:52:43 +0100

    bit opr small fix

--- bit_opr.cc
+++ bit_opr.cc
@@ -4,12 +4,12 @@
 template<int b>
 struct bit{enum{set=numtype(1)<<b,unset = !set};};
 template<int a, int b>
-struct bits{enum{set=(numtype(1)<<a)|bits<a-1,b>::set,unset = !set};};
+struct bits_in{enum{set=(numtype(1)<<a)|bits_in<a-1,b>::set,unset = !set};};
 template<int b>
-struct bits<b,b>{enum{set=bit<b>::set,unset = !set};};
+struct bits_in<b,b>{enum{set=bit<b>::set,unset = !set};};
 template<int a, int b>
-numtype occupy_bits(numtype small_value){
-	numtype cut=small_value&bits<a-b,0>::set;
+inline numtype occupy_bits(numtype small_value){
+	numtype cut=small_value&bits_in<a-b,0>::set;
 	if(small_value!=cut)throw;
 	return cut<<b;
 }
