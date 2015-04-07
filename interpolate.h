684fd37e5e89923e7a94c8b3c24c644f5d9fe3fb
alexkernphysiker <kernphysiker@openmailbox.org>
2015-04-08 01:09:48 +0200

    interpolate.h optimization

--- interpolate.h
+++ interpolate.h
@@ -25,7 +25,6 @@ int  WhereToInsert(int from, int to, indexer X, comparable x){
 	}
 	return end;
 }
-
 template<class comparable,class indexer, class Size, class Insert>
 void InsertSorted(comparable x,indexer X,Size size,Insert insert){
 	insert(WhereToInsert(0,size()-1,X,x),x);
@@ -34,7 +33,6 @@ void InsertSorted(comparable x,indexer X,Size size,Insert insert){
 #define std_insert(vector,type) [&vector](int pos,type x){vector.insert(vector.begin()+pos,x);}
 #define field_size(vector)  [this](){return vector.size();}
 #define field_insert(vector,type)  [this](int pos,type x){vector.insert(vector.begin()+pos,x);}
-
 template<class numX, class indexerX, class numY=numX, class indexerY=indexerX>
 numY  Interpolate_Linear(int from, int to, indexerX X, indexerY Y, numX x){
 	int i=WhereToInsert(from,to,X,x);
@@ -42,86 +40,12 @@ numY  Interpolate_Linear(int from, int to, indexerX X, indexerY Y, numX x){
 		throw;
 	if(i>to)
 		throw;
-	numY k=numY(x-X[i-1])/numY(X[i]-X[i-1]);
-	return Y[i-1]+k*(Y[i]-Y[i-1]);
+	numX k=(x-X[i-1])/(X[i]-X[i-1]);
+	return Y[i-1]+(Y[i]-Y[i-1])*numY(k);
 }
-template<class numt, class Size,class indexerX, class indexerY=indexerX>
-numt InterpolateLinear(numt x, indexerX X, indexerY Y,Size size){
+template<class numX, class Size,class indexerX, class numY, class indexerY=indexerX>
+numY InterpolateLinear(numX x, indexerX X, indexerY Y,Size size){
 	return Interpolate_Linear(0,size()-1,X,Y,x);
-}
-template<class numX, class numY=numX>
-class FuncTable{
-private:
-	numX* X;
-	numY* Y;
-	int cnt;
-public:
-	FuncTable(){cnt=2;X=new numX[cnt];Y=new numY[cnt];}
-	FuncTable(int sz){
-		if(sz<=1)throw;
-		cnt=sz;
-		X=new numX[cnt];
-		Y=new numY[cnt];
-	}
-	FuncTable(FuncTable &f){
-		cnt=f.cnt;
-		X=new numX[cnt];
-		Y=new numY[cnt];
-		for(int i=0; i<cnt;i++){
-			X[i]=(f.X[i]);
-			Y[i]=(f.Y[i]);
-		}
-	}
-	virtual ~FuncTable(){delete[] X; delete[] Y;}
-	void set(int i,numX x, numY y){X[i]=(x);Y[i]=(y);}
-	void sety(int i,numY y){Y[i]=(y);}
-	numX getx(int i){return X[i];}
-	numY gety(int i){return Y[i];}
-	int size(){return cnt;}
-	numY operator()(numX x){
-		return Interpolate_Linear<numX,numX*,numY,numY*>(0,cnt-1,X,Y,x);
-	}
-};
-template<class numX, class func, class numY=numX>
-void fillFuncTable(FuncTable<numX,numY> &tbl,numX from, numX to,func y){
-	numX step=(to-from)/(tbl.size()-1);
-	for(int i=0;i<tbl.size();i++){
-		numX x=from+(step*numX(i));
-		tbl.set(i,x,y(x));
-	}
-}
-template<class numX, class numY=numX>
-void fillFuncTableWithZeros(FuncTable<numX,numY> &tbl,numX from, numX to){
-	numX step=(to-from)/(tbl.size()-1);
-	for(int i=0;i<tbl.size();i++){
-		numX x=from+(step*numX(i));
-		tbl.set(i,x,numY(0));
-	}
-}
-template<class numt>
-void add2hist(FuncTable<numt> &tbl, numt value){
-	int index=tbl.size()-1;int beg=0;
-	if(value<tbl.getx(index)){
-		if(value>tbl.getx(beg)){
-			while(1<(index-beg)){
-				int mid=(beg+index)/2;
-				if(value<tbl.getx(mid))index=mid;else
-					if(value>=tbl.getx(mid))beg=mid;
-			}
-		}else index=0;
-	}else index=tbl.size();
-	if((tbl.size()-1)<=index){
-		int i=tbl.size()-1;
-		tbl.sety(i,tbl.gety(i)+1);
-	}
-	else if(0==index){
-		tbl.sety(0,tbl.gety(0)+1);
-	}else {
-	  double x1=tbl.getx(index-1);
-	  double x2=tbl.getx(index);
-	  if((value-x1)<(x2-value))index--;
-	  tbl.sety(index,tbl.gety(index)+1);
-	}
 }
 namespace details{
 	template<class numX, class numY=numX, class PairIndexer=std::vector<std::pair<numX,numY>>>
@@ -147,15 +71,16 @@ numY  Interpolate_Linear2(int from, int to, PairIndexer tbl, numX x){
 		throw;
 	if(i>to)
 		throw;
-	numY k=numY(x-tbl[i-1].first)/numY(tbl[i].first-tbl[i-1].first);
-	return tbl[i-1].second+k*(tbl[i].second-tbl[i-1].second);
+	numX k=(x-tbl[i-1].first)/(tbl[i].first-tbl[i-1].first);
+	return tbl[i-1].second+(tbl[i].second-tbl[i-1].second)*numY(k);
 }
-template<class numX, class numY, class PairIndexer,class Size>
+template<class numX, class numY=numX, class PairIndexer=std::vector<std::pair<numX,numY>>,class Size=std::function<int()>>
 numY InterpolateLinear2(numX x, PairIndexer tbl,Size size){
 	return Interpolate_Linear2(0,size()-1,tbl,x);
 }
 template<class numX, class numY=numX>
 class LinearInterpolation{
+public:
 	typedef std::pair<numX,numY> Point;
 private:
 	std::vector<Point> data;
@@ -166,11 +91,10 @@ public:
 		return *this;
 	}
 	numY operator()(numX x){
-		using namespace std;
 		using namespace details;
-		return InterpolateLinear2<numX,numY,vector<Point>,function<int()>>(x,data,field_size(data));
+		return InterpolateLinear2<numX,numY>(x,data,field_size(data));
 	}
-	Point operator[](int i){
+	Point& operator[](int i){
 		return data[i];
 	}
 	int size(){
