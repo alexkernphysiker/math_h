ce94a864a2fd70ae666dd28e1dfb564085461fd4
alexkernphysiker <kernphysiker@openmailbox.org>
2015-05-08 17:30:41 +0200

    InterpolateLinear test

--- math_h_tests/interpolate.cpp
+++ math_h_tests/interpolate.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include <vector>
+#include <math.h>
 #include <interpolate.h>
 using namespace std;
 double TestArray[]={-2,-1,0,1,2,3,4,5,6,7};
@@ -30,5 +31,81 @@ TEST(InsertSorted,BasicTest){
 		for(int j=0;j<i;j++)
 			ASSERT_TRUE(X[j]<=X[j+1]);
 	}
+}
+TEST(LinearInterpolation,Create){
+	LinearInterpolation<double> F;
+	EXPECT_EQ(0,F.size());
+	ASSERT_ANY_THROW(F.min());
+	ASSERT_ANY_THROW(F.max());
+	EXPECT_EQ(&F,&(F<<make_pair(0,0)));
+	EXPECT_EQ(1,F.size());
+	ASSERT_NO_THROW(F.min());
+	ASSERT_NO_THROW(F.max());
+	ASSERT_ANY_THROW(F(0.5));
+	EXPECT_EQ(&F,&(F<<make_pair(1,0)));
+	EXPECT_EQ(2,F.size());
+	ASSERT_NO_THROW(F.min());
+	ASSERT_NO_THROW(F.max());
+	EXPECT_EQ(0,F(0.5));
+}
+#define _EQ(a,b) EXPECT_TRUE(pow(a-b,2)<0.0001)
+TEST(LinearInterpolation,SimpleLine){
+	LinearInterpolation<double> F;
+	F<<make_pair(0,0)<<make_pair(1,1);
+	EXPECT_EQ(0,F(0));
+	EXPECT_EQ(1,F(1));
+	_EQ(0.1,F(0.1));
+	_EQ(0.2,F(0.2));
+	_EQ(0.3,F(0.3));
+	_EQ(0.4,F(0.4));
+	_EQ(0.5,F(0.5));
+	_EQ(0.6,F(0.6));
+	_EQ(0.7,F(0.7));
+	_EQ(0.8,F(0.8));
+	_EQ(0.9,F(0.9));
+	F<<make_pair(2,0);
+	EXPECT_EQ(0,F(0));
+	EXPECT_EQ(1,F(1));
+	_EQ(0.1,F(0.1));
+	_EQ(0.2,F(0.2));
+	_EQ(0.3,F(0.3));
+	_EQ(0.4,F(0.4));
+	_EQ(0.5,F(0.5));
+	_EQ(0.6,F(0.6));
+	_EQ(0.7,F(0.7));
+	_EQ(0.8,F(0.8));
+	_EQ(0.9,F(0.9));
+	EXPECT_EQ(0,F(2));
+	_EQ(0.1,F(1.9));
+	_EQ(0.2,F(1.8));
+	_EQ(0.3,F(1.7));
+	_EQ(0.4,F(1.6));
+	_EQ(0.5,F(1.5));
+	_EQ(0.6,F(1.4));
+	_EQ(0.7,F(1.3));
+	_EQ(0.8,F(1.2));
+	_EQ(0.9,F(1.1));
+	F<<make_pair(0.5,0.5)<<make_pair(1.5,0.5);
+	EXPECT_EQ(0,F(0));
+	EXPECT_EQ(1,F(1));
+	_EQ(0.1,F(0.1));
+	_EQ(0.2,F(0.2));
+	_EQ(0.3,F(0.3));
+	_EQ(0.4,F(0.4));
+	EXPECT_EQ(0.5,F(0.5));
+	_EQ(0.6,F(0.6));
+	_EQ(0.7,F(0.7));
+	_EQ(0.8,F(0.8));
+	_EQ(0.9,F(0.9));
+	EXPECT_EQ(0,F(2));
+	_EQ(0.1,F(1.9));
+	_EQ(0.2,F(1.8));
+	_EQ(0.3,F(1.7));
+	_EQ(0.4,F(1.6));
+	EXPECT_EQ(0.5,F(1.5));
+	_EQ(0.6,F(1.4));
+	_EQ(0.7,F(1.3));
+	_EQ(0.8,F(1.2));
+	_EQ(0.9,F(1.1));
 }
 
