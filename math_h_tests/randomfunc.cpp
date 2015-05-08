43945a96ccdf079f0b3ea193426e1df2b2e6ee56
alexkernphysiker <kernphysiker@openmailbox.org>
2015-05-08 18:11:23 +0200

    RandomGauss test

--- math_h_tests/randomfunc.cpp
+++ math_h_tests/randomfunc.cpp
@@ -29,3 +29,30 @@ TEST(RandomUniformlyR,Throwing){
 		for(double r=-10;r<l;r+=0.5)
 			ASSERT_ANY_THROW(RandomUniformlyR(l,r));
 }
+TEST(RandomGauss,BasicTest){
+	for(double X=-10;X<=10;X+=0.5){
+		for(int c=0;c<100;c++){
+			double V=RandomGauss(0.0,X);
+			EXPECT_EQ(X,V);
+		}
+		for(double sigma=0.1;sigma<3;sigma+=0.1){
+			int cg=0,cl=0;
+			for(int i=0;i<100;i++){
+				double V=RandomGauss(sigma,X);
+				if(pow(V-X,2)<pow(sigma,2))
+					cl++;
+				else
+					cg++;
+			}
+			EXPECT_TRUE(cg<cl);
+			EXPECT_TRUE(cg*6>cl);
+		}
+	}
+}
+TEST(RandomGauss,Throwing){
+	for(double X=-1;X<=1;X+=0.5){
+		auto f=[&X](){return RandomGauss(-1.0,X);};
+		ASSERT_ANY_THROW(f());
+	}
+}
+
