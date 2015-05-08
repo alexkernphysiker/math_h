762023c3a1fe280e5f6babe51b2934ab9dee3ad3
alexkernphysiker <kernphysiker@openmailbox.org>
2015-05-08 16:36:58 +0200

    WhereToInsert tests

---
+++ math_h_tests/interpolate.cpp
@@ -0,0 +1,25 @@
+#include <gtest/gtest.h>
+#include <functional>
+#include <interpolate.h>
+using namespace std;
+double TestArray[]={-2,-1,0,1,2,3,4,5,6,7};
+TEST(WhereToInsert,BorderConditions){
+	for(int beg=0;beg<10;beg++)
+		for(double V=TestArray[beg]-0.5;V<=TestArray[beg]+0.5;V+=0.5){
+			int index=WhereToInsert<double,double*>(beg,beg-1,TestArray,V);
+			EXPECT_EQ(beg,index);
+			index=WhereToInsert<double,double*>(beg,beg,TestArray,V);
+			if(V<TestArray[beg])EXPECT_EQ(beg,index);
+			if(V>TestArray[beg])EXPECT_EQ(beg+1,index);
+			if(V==TestArray[beg])ASSERT_TRUE((index==beg)||(index==(beg+1)));
+		}
+}
+TEST(WhereToInsert,NormalConditions){
+	for(double x=TestArray[0]-0.5;x<=TestArray[9]+0.5;x+=0.5)
+		for(int beg=0;beg<10;beg++)for(int end=beg+1;end<10;end++){
+			int index=WhereToInsert(beg,end,TestArray,x);
+			if(x<TestArray[beg])EXPECT_EQ(beg,index);
+			else if(x>TestArray[end])EXPECT_EQ(end+1,index);
+			else ASSERT_TRUE((x>=TestArray[index-1])&&(x<=TestArray[index]));
+		}
+}
