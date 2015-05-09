de9aaf55fbc0bd25574aae43aa93127486f58c69
alexkernphysiker <kernphysiker@openmailbox.org>
2015-05-09 13:27:54 +0200

    sigma tests

---
+++ math_h_tests/sigma.cpp
@@ -0,0 +1,33 @@
+#include <gtest/gtest.h>
+#include <sigma.h>
+#include <randomfunc.h>
+using namespace std;
+TEST(Sigma,Throwing){
+	Sigma<double> S;
+	EXPECT_EQ(0,S.count());
+	EXPECT_THROW(S.getAverage(),exception);
+	EXPECT_THROW(S.getSigmaSqr(),exception);
+	EXPECT_THROW(S.getSigma(),exception);
+}
+#define _EQ(a,b) EXPECT_TRUE(pow(a-b,2)<0.01)
+TEST(Sigma,Base){
+	Sigma<double> S;
+	S.AddValue(0);
+	EXPECT_EQ(1,S.count());
+	EXPECT_EQ(0,S.getAverage());
+	EXPECT_THROW(S.getSigmaSqr(),exception);
+	EXPECT_THROW(S.getSigma(),exception);
+	S.AddValue(1);
+	EXPECT_EQ(2,S.count());
+	EXPECT_EQ(0.5,S.getAverage());
+	EXPECT_EQ(0.5,S.getSigmaSqr());
+	EXPECT_EQ(S.getSigma(),sqrt(S.getSigmaSqr()));
+}
+TEST(Sigma,WithRandomValues){
+	Sigma<double> S;
+	for(int i=0;i<1000;i++)
+		S.AddValue(RandomGauss(1.0,1.0));
+	_EQ(1.0,S.getAverage());
+	_EQ(1.0,S.getSigma());
+}
+
