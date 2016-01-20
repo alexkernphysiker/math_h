32bba18a7eea69a23fd880bbcc86fcbba841e3a3
alexkernphysiker <kernphysiker@openmailbox.org>
2016-01-20 13:03:31 +0100

    namespace GnuplotWrap

--- gnuplot_wrap/gnuplot_wrap.cpp
+++ gnuplot_wrap/gnuplot_wrap.cpp
@@ -1,67 +1,68 @@
 // this file is distributed under 
 // MIT license
 #include <gnuplot_wrap.h>
-using namespace std;
-string ReplaceAll(const string&source, const string& from, const string& to) {
-	string str=source;
-	size_t start_pos = 0;
-	while((start_pos = str.find(from, start_pos)) != string::npos) {
-		str.replace(start_pos, from.length(), to);
-		start_pos += to.length();
+namespace GnuplotWrap{
+	using namespace std;
+	string ReplaceAll(const string&source, const string& from, const string& to) {
+		string str=source;
+		size_t start_pos = 0;
+		while((start_pos = str.find(from, start_pos)) != string::npos) {
+			str.replace(start_pos, from.length(), to);
+			start_pos += to.length();
+		}
+		return str;
 	}
-	return str;
-}
-string ReplaceAll(string&& str, const string& from, const string& to){
-	return ReplaceAll(str,from,to);
-}
-
-Plotter::Plotter(){
-	counter=0;
-	outpath="*";
-}
-Plotter::~Plotter(){
-	ofstream script;
-	string name=m_prefix+"_script.gnuplot";
-	script.open((outpath+"/"+name).c_str());
-	if(script.is_open()){
-		for(string line:lines)
-			script<<line<<"\n";
-		script.close();
-		name=string("gnuplot ")+name;
-		string old=getcwd(NULL,0);
-		chdir(outpath.c_str());
-		system(name.c_str());
-		chdir(old.c_str());
+	string ReplaceAll(string&& str, const string& from, const string& to){
+		return ReplaceAll(str,from,to);
 	}
 	
-}
-Plotter &Plotter::Instance(){
-	static Plotter m_instance;
-	return m_instance;
-}
-void Plotter::SetOutput(string&&out,string&&prefix){
-	if(outpath!="*")
-		throw;
-	outpath=out;
-	m_prefix=prefix;
-}
-string&Plotter::OutPath()const{
-	if(outpath=="*")
-		throw;
-	return const_cast<string&>(outpath);
-}
-string&Plotter::Prefix()const{
-	if(outpath=="*")
-		throw;
-	return const_cast<string&>(m_prefix);
-}
-string Plotter::GetTerminal(){
-	counter++;
-	return string("set terminal pngcairo size 1024,868 font 'Verdana,18'\nset output '")+m_prefix+to_string(counter)+".png'";
-}
-Plotter &Plotter::operator<<(const string&line){
-	lines.push_back(line);
-	return *this;
-}
-Plotter& Plotter::operator<<(string&&line){return operator<<(line);}
-
+	Plotter::Plotter(){
+		counter=0;
+		outpath="*";
+	}
+	Plotter::~Plotter(){
+		ofstream script;
+		string name=m_prefix+"_script.gnuplot";
+		script.open((outpath+"/"+name).c_str());
+		if(script.is_open()){
+			for(string line:lines)
+				script<<line<<"\n";
+			script.close();
+			name=string("gnuplot ")+name;
+			string old=getcwd(NULL,0);
+			chdir(outpath.c_str());
+			system(name.c_str());
+			chdir(old.c_str());
+		}
+		
+	}
+	Plotter &Plotter::Instance(){
+		static Plotter m_instance;
+		return m_instance;
+	}
+	void Plotter::SetOutput(string&&out,string&&prefix){
+		if(outpath!="*")
+			throw;
+		outpath=out;
+		m_prefix=prefix;
+	}
+	string&Plotter::OutPath()const{
+		if(outpath=="*")
+			throw;
+		return const_cast<string&>(outpath);
+	}
+	string&Plotter::Prefix()const{
+		if(outpath=="*")
+			throw;
+		return const_cast<string&>(m_prefix);
+	}
+	string Plotter::GetTerminal(){
+		counter++;
+		return string("set terminal pngcairo size 1024,868 font 'Verdana,18'\nset output '")+m_prefix+to_string(counter)+".png'";
+	}
+	Plotter &Plotter::operator<<(const string&line){
+		lines.push_back(line);
+		return *this;
+	}
+	Plotter& Plotter::operator<<(string&&line){return operator<<(line);}
+};
--- include/gnuplot_wrap.h
+++ include/gnuplot_wrap.h
@@ -14,139 +14,142 @@
 #include <math.h>
 #include <exception>
 #include <math_h/interpolate.h>
-std::string ReplaceAll(const std::string&str, const std::string& from, const std::string& to);
-std::string ReplaceAll(std::string&&str, const std::string& from, const std::string& to);
-class Plotter{
-public:
-	Plotter();
-	~Plotter();
-	static Plotter &Instance();
-	void SetOutput(std::string&&out,std::string&&prefix="");
-	std::string&OutPath()const;
-	std::string&Prefix()const;
-	std::string GetTerminal();
-	Plotter &operator<<(const std::string&line);
-	Plotter &operator<<(std::string&&line);
-private:
-	std::vector<std::string> lines;
-	unsigned int counter;
-	std::string outpath;
-	std::string m_prefix;
-};
-template<class numt>class Plot{
-private:
-	std::vector<std::string> lines;
-	std::vector<std::string> plots;
-public:
-	typedef std::function<numt(numt)> FUNC;
-	typedef std::function<void(std::ofstream&)> PLOTOUTPUT;
-	Plot&operator<<(const std::string&line){
-		lines.push_back(line);
-		return *this;
-	}
-	Plot&operator<<(std::string&&line){return operator<<(line);}
-	Plot(){
-		operator<<(Plotter::Instance().GetTerminal());
-	}
-	virtual ~Plot(){
-		for(std::string&line:lines)
-			Plotter::Instance()<<line;
-		for(int i=0,n=plots.size();i<n;i++){
-			std::string line=plots[i];
-			if(i==0)
-				line="plot "+line;
-			if(i<(n-1))
-				line+=",\\";
-			Plotter::Instance()<<line;
+namespace GnuplotWrap{
+	using namespace std;
+	std::string ReplaceAll(const std::string&str, const std::string& from, const std::string& to);
+	std::string ReplaceAll(std::string&&str, const std::string& from, const std::string& to);
+	class Plotter{
+	public:
+		Plotter();
+		~Plotter();
+		static Plotter &Instance();
+		void SetOutput(string&&out,string&&prefix="");
+		string&OutPath()const;
+		string&Prefix()const;
+		string GetTerminal();
+		Plotter &operator<<(const string&line);
+		Plotter &operator<<(string&&line);
+	private:
+		vector<string> lines;
+		unsigned int counter;
+		string outpath;
+		string m_prefix;
+	};
+	template<class numt>class Plot{
+	private:
+		vector<string> lines;
+		vector<string> plots;
+	public:
+		typedef function<numt(numt)> FUNC;
+		typedef function<void(ofstream&)> PLOTOUTPUT;
+		Plot&operator<<(const string&line){
+			lines.push_back(line);
+			return *this;
 		}
-	}
-public:
-	Plot& Object(std::string&&plot){
-		plots.push_back(plot);
-		return *this;
-	}
-	Plot& File(std::string&&name,std::string&&title,std::string&&descr){
-		std::string line="\""+name+"\" ";
-		line+=descr;
-		line+=" title \"";
-		line+=title;
-		line+="\"";
-		return Object(static_cast<std::string&&>(line));
-	}
-	Plot&OutputPlot(std::string&&name,PLOTOUTPUT delegate,std::string&&description){
-		std::ofstream data;
-		std::string filename=ReplaceAll(ReplaceAll(name," ","_"),"=","_")+".txt";
-		data.open((Plotter::Instance().OutPath()+"/"+filename).c_str());
-		if(data.is_open()){
-			delegate(data);
-			File(
-				static_cast<std::string&&>(filename),
-				static_cast<std::string&&>(name),
-				static_cast<std::string&&>(description)
-			);
-			data.close();
+		Plot&operator<<(string&&line){return operator<<(line);}
+		Plot(){
+			operator<<(Plotter::Instance().GetTerminal());
 		}
-		return *this;
-	}
-	Plot &Hist(std::string&&name,const LinearInterpolation_fixedsize<numt>&points){
-		OutputPlot(static_cast<std::string&&>(name),[this,&points](std::ofstream&data){
-			for(int i=0,n=points.size();i<n;i++)
-				data<<points.getX(i)<<" "<<points.getY(i)<<"\n";
-		},"using 1:2");
-		return *this;
-	}
-	Plot &HistWithStdError(std::string&&name,const LinearInterpolation_fixedsize<numt>&points){
-		OutputPlot(static_cast<std::string&&>(name),[&points](std::ofstream&data){
-			for(int i=0,n=points.size();i<n;i++){
-				double y=points.getY(i);
-				if(y<0)throw std::exception();
-				   double dy=sqrt(y);if(y<1)y=1;
-				data<<points.getX(i)<<" "<<y<<" "<<dy<<"\n";
+		virtual ~Plot(){
+			for(string&line:lines)
+				Plotter::Instance()<<line;
+			for(int i=0,n=plots.size();i<n;i++){
+				string line=plots[i];
+				if(i==0)
+					line="plot "+line;
+				if(i<(n-1))
+					line+=",\\";
+				Plotter::Instance()<<line;
 			}
-		},"using 1:2:($2-$3):($2+$3) with yerrorbars");
-		return *this;
-	}
-	Plot &Line(std::string&&name,FUNC func,numt from,numt to,numt step){
-		OutputPlot(static_cast<std::string&&>(name),[func,from,to,step](std::ofstream&data){
-			for(double x=from;x<=to;x+=step)
-				data<<x<<" "<<func(x)<<"\n";
-		},"w l");
-		return *this;
-	}
-};
-template<class numt,class Indexer>class PlotPoints:public Plot<numt>{
-public:
-	typedef std::function<numt(numt)> FUNC;
-	typedef std::pair<numt,numt> PAIR;
-	PlotPoints():Plot<numt>(){}
-	virtual ~PlotPoints(){}
-	PlotPoints &LineOnly(std::string&&name,const Indexer&points){
-		Plot<numt>::OutputPlot(static_cast<std::string&&>(name),[&points](std::ofstream&data){
-			for(PAIR p:points)
-				data<<p.first<<" "<<p.second<<"\n";
-		},"w l");
-		return *this;
-	}
-	PlotPoints &WithoutErrors(std::string&&name,const Indexer&points){
-		Plot<numt>::OutputPlot(static_cast<std::string&&>(name),[&points](std::ofstream&data){
-			for(PAIR p:points)
-				data<<p.first<<" "<<p.second<<"\n";
-		},"using 1:2");
-		return *this;
-	}
-	PlotPoints &WithErrorOnX(std::string&&name,const Indexer&points,FUNC error){
-		Plot<numt>::OutputPlot(static_cast<std::string&&>(name),[&points,error](std::ofstream&data){
-			for(auto p:points)
-				data<<p.first<<" "<<p.second<<" "<<error(p.first)<<"\n";
-		},"using 1:2:($2-$3):($2+$3) with yerrorbars");
-		return *this;
-	}
-	PlotPoints &WithErrorOnY(std::string&&name,const Indexer&points,FUNC error){
-		Plot<numt>::OutputPlot(static_cast<std::string&&>(name),[&points,error](std::ofstream&data){
-			for(auto p:points)
-				data<<p.first<<" "<<p.second<<" "<<error(p.second)<<"\n";
-		},"using 1:2:($2-$3):($2+$3) with yerrorbars");
-		return *this;
-	}
+		}
+	public:
+		Plot& Object(string&&plot){
+			plots.push_back(plot);
+			return *this;
+		}
+		Plot& File(string&&name,string&&title,string&&descr){
+			string line="\""+name+"\" ";
+			line+=descr;
+			line+=" title \"";
+			line+=title;
+			line+="\"";
+			return Object(static_cast<std::string&&>(line));
+		}
+		Plot&OutputPlot(string&&name,PLOTOUTPUT delegate,string&&description){
+			ofstream data;
+			string filename=ReplaceAll(ReplaceAll(name," ","_"),"=","_")+".txt";
+			data.open((Plotter::Instance().OutPath()+"/"+filename).c_str());
+			if(data.is_open()){
+				delegate(data);
+				File(
+					static_cast<std::string&&>(filename),
+				     static_cast<std::string&&>(name),
+				     static_cast<std::string&&>(description)
+				);
+				data.close();
+			}
+			return *this;
+		}
+		Plot &Hist(string&&name,const LinearInterpolation_fixedsize<numt>&points){
+			OutputPlot(static_cast<string&&>(name),[this,&points](ofstream&data){
+				for(int i=0,n=points.size();i<n;i++)
+					data<<points.getX(i)<<" "<<points.getY(i)<<"\n";
+			},"using 1:2");
+			return *this;
+		}
+		Plot &HistWithStdError(string&&name,const LinearInterpolation_fixedsize<numt>&points){
+			OutputPlot(static_cast<string&&>(name),[&points](ofstream&data){
+				for(int i=0,n=points.size();i<n;i++){
+					double y=points.getY(i);
+					if(y<0)throw std::exception();
+				   double dy=sqrt(y);if(y<1)y=1;
+				   data<<points.getX(i)<<" "<<y<<" "<<dy<<"\n";
+				}
+			},"using 1:2:($2-$3):($2+$3) with yerrorbars");
+			return *this;
+		}
+		Plot &Line(string&&name,FUNC func,numt from,numt to,numt step){
+			OutputPlot(static_cast<string&&>(name),[func,from,to,step](ofstream&data){
+				for(double x=from;x<=to;x+=step)
+					data<<x<<" "<<func(x)<<"\n";
+			},"w l");
+			return *this;
+		}
+	};
+	template<class numt,class Indexer>class PlotPoints:public Plot<numt>{
+	public:
+		typedef std::function<numt(numt)> FUNC;
+		typedef std::pair<numt,numt> PAIR;
+		PlotPoints():Plot<numt>(){}
+		virtual ~PlotPoints(){}
+		PlotPoints &LineOnly(string&&name,const Indexer&points){
+			Plot<numt>::OutputPlot(static_cast<string&&>(name),[&points](ofstream&data){
+				for(PAIR p:points)
+					data<<p.first<<" "<<p.second<<"\n";
+			},"w l");
+			return *this;
+		}
+		PlotPoints &WithoutErrors(string&&name,const Indexer&points){
+			Plot<numt>::OutputPlot(static_cast<string&&>(name),[&points](ofstream&data){
+				for(PAIR p:points)
+					data<<p.first<<" "<<p.second<<"\n";
+			},"using 1:2");
+			return *this;
+		}
+		PlotPoints &WithErrorOnX(string&&name,const Indexer&points,FUNC error){
+			Plot<numt>::OutputPlot(static_cast<string&&>(name),[&points,error](ofstream&data){
+				for(auto p:points)
+					data<<p.first<<" "<<p.second<<" "<<error(p.first)<<"\n";
+			},"using 1:2:($2-$3):($2+$3) with yerrorbars");
+			return *this;
+		}
+		PlotPoints &WithErrorOnY(string&&name,const Indexer&points,FUNC error){
+			Plot<numt>::OutputPlot(static_cast<string&&>(name),[&points,error](ofstream&data){
+				for(auto p:points)
+					data<<p.first<<" "<<p.second<<" "<<error(p.second)<<"\n";
+			},"using 1:2:($2-$3):($2+$3) with yerrorbars");
+			return *this;
+		}
+	};
 };
 #endif
