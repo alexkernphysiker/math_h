a9467b46abc9322e3dde3e4e5832918a185f197c
alexkernphysiker <kernphysiker@openmailbox.org>
2016-02-06 22:46:37 +0100

    plot library update

--- include/gnuplot_wrap.h
+++ include/gnuplot_wrap.h
@@ -68,15 +68,15 @@ namespace GnuplotWrap{
 			plots.push_back(plot);
 			return *this;
 		}
-		Plot& File(string&&name,string&&title,string&&descr){
+		Plot& File(string&&name,string&&options,string&&title=""){
 			string line="\""+name+"\" ";
-			line+=descr;
+			line+=options;
 			line+=" title \"";
 			line+=title;
 			line+="\"";
 			return Object(static_cast<std::string&&>(line));
 		}
-		Plot&OutputPlot(string&&name,PLOTOUTPUT delegate,string&&description){
+		Plot&OutputPlot(PLOTOUTPUT delegate,string&&options,string&&title=""){
 			ofstream data;
 			string filename=Plotter::Instance().GetFileName();
 			data.open((Plotter::Instance().OutPath()+"/"+filename).c_str());
@@ -84,36 +84,36 @@ namespace GnuplotWrap{
 				delegate(data);
 				File(
 					static_cast<std::string&&>(filename),
-					static_cast<std::string&&>(name),
-					static_cast<std::string&&>(description)
+					static_cast<std::string&&>(options),
+					static_cast<std::string&&>(title)
 				);
 				data.close();
 			}
 			return *this;
 		}
-		Plot &Hist(string&&name,const LinearInterpolation_fixedsize<numt>&points){
-			OutputPlot(static_cast<string&&>(name),[this,&points](ofstream&data){
+		Plot &Hist(const LinearInterpolation_fixedsize<numt>&points,string&&title=""){
+			OutputPlot([this,&points](ofstream&data){
 				for(int i=0,n=points.size();i<n;i++)
 					data<<points.getX(i)<<" "<<points.getY(i)<<endl;
-			},"using 1:2");
+			},"using 1:2",static_cast<string&&>(title));
 			return *this;
 		}
-		Plot &HistWithStdError(string&&name,const LinearInterpolation_fixedsize<numt>&points){
-			OutputPlot(static_cast<string&&>(name),[&points](ofstream&data){
+		Plot &HistWithStdError(const LinearInterpolation_fixedsize<numt>&points,string&&title=""){
+			OutputPlot([&points](ofstream&data){
 				for(int i=0,n=points.size();i<n;i++){
 					double y=points.getY(i);
 					if(y<0)throw std::exception();
 				   double dy=sqrt(y);if(y<1)y=1;
 				   data<<points.getX(i)<<" "<<y<<" "<<dy<<endl;
 				}
-			},"using 1:2:($2-$3):($2+$3) with yerrorbars");
+			},"using 1:2:($2-$3):($2+$3) with yerrorbars",static_cast<string&&>(title));
 			return *this;
 		}
-		Plot &Line(string&&name,FUNC func,numt from,numt to,numt step){
-			OutputPlot(static_cast<string&&>(name),[func,from,to,step](ofstream&data){
+		Plot &Line(FUNC func,numt from,numt to,numt step,string&&title=""){
+			OutputPlot([func,from,to,step](ofstream&data){
 				for(double x=from;x<=to;x+=step)
 					data<<x<<" "<<func(x)<<endl;
-			},"w l");
+			},"w l",static_cast<string&&>(title));
 			return *this;
 		}
 	};
@@ -122,18 +122,18 @@ namespace GnuplotWrap{
 		typedef std::pair<numt,numt> POINT;
 		PlotPoints():Plot<numt>(){}
 		virtual ~PlotPoints(){}
-		PlotPoints &Line(string&&name,const Indexer&points){
-			Plot<numt>::OutputPlot(static_cast<string&&>(name),[&points](ofstream&data){
+		PlotPoints &Line(const Indexer&points,string&&title=""){
+			Plot<numt>::OutputPlot([&points](ofstream&data){
 				for(POINT p:points)
 					data<<p.first<<" "<<p.second<<endl;
-			},"w l");
+			},"w l",static_cast<string&&>(title));
 			return *this;
 		}
-		PlotPoints &Points(string&&name,const Indexer&points){
-			Plot<numt>::OutputPlot(static_cast<string&&>(name),[&points](ofstream&data){
+		PlotPoints &Points(const Indexer&points,string&&title=""){
+			Plot<numt>::OutputPlot([&points](ofstream&data){
 				for(POINT p:points)
 					data<<p.first<<" "<<p.second<<endl;
-			},"using 1:2");
+			},"using 1:2",static_cast<string&&>(title));
 			return *this;
 		}
 	};
@@ -142,11 +142,11 @@ namespace GnuplotWrap{
 		typedef std::pair<value<double>,value<double>> POINT;
 		PlotValues():Plot<numt>(){}
 		virtual ~PlotValues(){}
-		PlotValues &Points(string&&name,const Indexer&points){
-			Plot<numt>::OutputPlot(static_cast<string&&>(name),[&points](ofstream&data){
+		PlotValues &Points(const Indexer&points,string&&title=""){
+			Plot<numt>::OutputPlot([&points](ofstream&data){
 				for(POINT p:points)
 					data<<p.first.val()<<" "<<p.first.delta()<<" "<<p.second.val()<<" "<<p.second.delta()<<endl;
-			},"using 1:3:($1-$2):($1+$2):($3-$4):($3+$4) with xyerrorbars");
+			},"using 1:3:($1-$2):($1+$2):($3-$4):($3+$4) with xyerrorbars",static_cast<string&&>(title));
 			return *this;
 		}
 	};
