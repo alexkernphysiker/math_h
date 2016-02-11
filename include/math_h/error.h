82409da069f301d8aa5f2c81f8747abee10d29ba
alexkernphysiker <kernphysiker@openmailbox.org>
2016-02-11 15:48:18 +0100

    error.h update

--- include/math_h/error.h
+++ include/math_h/error.h
@@ -5,12 +5,13 @@
 #include <exception>
 #include <string>
 namespace MathTemplates{
+	using namespace std;
 	template<typename SOURCE, int code=0>
-	class Exception:public std::exception{
+	class Exception:public exception{
 	private:
-		std::string m_msg;
+		string m_msg;
 	public:
-		Exception(std::string msg){m_msg=msg;}
+		Exception(string msg){m_msg=msg;}
 		virtual ~Exception() throw(){}
 		virtual const char* what() const throw(){return m_msg.c_str();}
 	};
