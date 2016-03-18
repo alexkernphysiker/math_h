6603c78e0c85d29f447889065bb453f309ea27ac
alexkernphysiker <kernphysiker@openmailbox.org>
2016-03-18 23:52:58 +0100

    example 3

--- Example/interpolation.cpp
+++ Example/interpolation.cpp
@@ -6,14 +6,13 @@ using namespace std;
 using namespace MathTemplates;
 using namespace GnuplotWrap;
 int main(){
-	Plotter::Instance().SetOutput(".","interpolation");
-	Plot<double> output;
-	
 	LinearInterpolation<double> test;
 	test<<point<double>(0,0)<<point<double>(1,1)<<point<double>(2,4)<<point<double>(3,9);
 	test<<point<double>(-1,1)<<point<double>(-2,4)<<point<double>(-3,9);
 	
 	auto x_chain=ChainWithStep(-3.0,0.1,3.0);
+	Plotter::Instance().SetOutput(".","interpolation");
+	Plot<double> output;
 	output.Line(SortedPoints<double>(test.func(),x_chain),"y");
 	output.Line(SortedPoints<double>(LinearInterpolation<double>(test*2.0).func(),x_chain),"y*2");
 	
---
+++ Example/sigma.cpp
@@ -0,0 +1,27 @@
+// this file is distributed under 
+// MIT license
+#include <iostream>
+#include <random>
+#include <math_h/sigma.h>
+#include <math_h/structures.h>
+#include <gnuplot_wrap.h>
+using namespace std;
+using namespace MathTemplates;
+using namespace GnuplotWrap;
+int main(){
+	mt19937 engine;
+	normal_distribution<double> d1(2,1);
+	normal_distribution<double> d2(5,1);
+	Sigma<double> S1,S2,S_sum;
+	
+	for(size_t i=0;i<5000;i++){
+		double a=d1(engine),b=d2(engine);
+		S1<<a;S2<<b;S_sum<<(a+b);
+	}
+	
+	cout<<"magnitude 1 = "<<S1().val()<<"+/-"<<S1().delta()<<endl;
+	cout<<"magnitude 2 = "<<S2().val()<<"+/-"<<S2().delta()<<endl;
+	auto th=S1()+S2();
+	cout<<"theory sum = "<<th.val()<<"+/-"<<th.delta()<<endl;
+	cout<<"experiment sum = "<<S_sum().val()<<"+/-"<<S_sum().delta()<<endl;
+}
