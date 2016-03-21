10fbb066e1cd287de1f4ad77ffe2b9e6c59050e0
alexkernphysiker <kernphysiker@openmailbox.org>
2016-03-21 20:32:23 +0100

    example update

--- Example/interpolation.cpp
+++ Example/interpolation.cpp
@@ -16,11 +16,11 @@ int main(){
 	output.Line(SortedPoints<double>(test.func(),x_chain),"y");
 	output.Line(SortedPoints<double>(LinearInterpolation<double>(test*2.0).func(),x_chain),"y*2");
 	
-	BiLinearInterpolation<double> test2({0.0,1.0,2.0,3.0},{0.0,1.0,2.0,3.0});
+	BiLinearInterpolation<double> test2({0.0,1.0,2.0,3.0,4.0},{0.0,1.0,2.0,3.0,4.0});
 	test2.FullCycleVar([](const double&x,const double&y,double&z){
-		z=(pow(x,2)+pow(y,2));
+		z=sin(sqrt(pow(x,2)+pow(y,2))/2.0);
 	});
-	BiSortedPoints<double> for_plot(ChainWithStep(0.1,0.1,2.9),ChainWithStep(0.1,0.1,2.9));
+	BiSortedPoints<double> for_plot(ChainWithStep(0.1,0.1,3.9),ChainWithStep(0.1,0.1,3.9));
 	for_plot.FullCycleVar([&test2](const double&x,const double&y,double&z){
 		z=test2(x,y);
 	});
