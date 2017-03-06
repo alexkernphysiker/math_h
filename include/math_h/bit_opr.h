1b1c6f628224d024f2830a318238f1de09c44965
alexkernphysiker <kernphysiker@openmailbox.org>
2017-03-06 12:14:18 +0100

    removed files

--- include/math_h/bit_opr.h
+++
@@ -1,9 +0,0 @@
-// this file is distributed under 
-// MIT license
-#ifndef ZRONCKJHLREWTQTI
-#define ZRONCKJHLREWTQTI
-namespace MathTemplates{
-	typedef long unsigned int numtype;
-	#include "bit_opr.cc"
-}
-#endif
