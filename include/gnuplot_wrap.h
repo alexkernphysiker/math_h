0718d34efb2903694fe5f8649950b153ef1c3a86
rundel <alexrndl@gmail.com>
2017-09-15 23:09:26 +0200

    fix

--- include/gnuplot_wrap.h
+++ include/gnuplot_wrap.h
@@ -67,7 +67,7 @@ protected:
             terminal_counter++;
             auto cnt = std::to_string(terminal_counter);
             while (cnt.length() < 5)cnt = "0" + cnt;
-            return firstline + "set output "
+            return firstline + "set output z."
 	+std::to_string(width)+"."+std::to_string(height)+"."+
 	m_prefix + "-plot-" + cnt + ".png'";
         } else {
