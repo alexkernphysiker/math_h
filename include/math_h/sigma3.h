958214f9b05e2b7737b11419938cfac0b4636366
rundel <alexrndl@gmail.com>
2018-05-15 00:01:28 +0300

    values with several uncertainties

---
+++ include/math_h/sigma3.h
@@ -0,0 +1,243 @@
+// this file is distributed under
+// LGPLv3 license
+#ifndef _____EXTENDED_UNCERTAINTIES_ESTIMATION_MORE_THAN_ONE_UNCERTAINTY_____________
+#define _____EXTENDED_UNCERTAINTIES_ESTIMATION_MORE_THAN_ONE_UNCERTAINTY_____________
+#include <memory>
+#include <math.h>
+#include "error.h"
+#include "sigma.h"
+#include "tabledata.h"
+namespace MathTemplates
+{
+template<size_t size, class numt = double>class Uncertainties;
+template<class numt>
+class Uncertainties<1, numt>
+{
+    template<size_t sizef, class n>friend class Uncertainties;
+public:
+    enum {UncertaintiesCount = 1};
+    typedef numt NumberType;
+private:
+    numt m_value,m_uncertainty;
+public:
+    virtual ~Uncertainties() {}
+    inline std::tuple<numt> to_tuple()const
+    {
+        return std::make_tuple(m_value,m_uncertainty);
+    }
+    template<class... Args>
+    inline Uncertainties(const std::tuple<Args...> &v): m_value(std::get<0>(v)),m_uncertainty(std::get<1>(v)) {}
+    template<class Arg>
+    inline Uncertainties(const Arg&arg):m_value(arg),m_uncertainty(0){}
+    template<class Arg,class Arg2>
+    inline Uncertainties(const Arg&arg,const Arg2&arg2):m_value(arg),m_uncertainty(arg2){}
+    template<class numt2>
+    inline Uncertainties(const Uncertainties<UncertaintiesCount,numt2>&V):m_value(V.m_value),m_uncertainty(V.m_uncertainty){}
+#ifdef ____full_version_of_math_h_____
+    template<size_t index>
+    inline const NumberType&uncertainty()const
+    {
+	static_assert(index == 1,"uncertainty index is out of range");
+        return m_uncertainty;
+    }
+#else
+    template<size_t index>
+    const NumberType&uncertainty()const
+    {
+	static_assert(index>0,"uncertainty index is out of range");
+	if(index>1)throw Exception<Uncertainties>("uncertainty index is out of range");
+        return m_uncertainty;
+    }
+#endif
+    inline const NumberType&val()const{return m_value;}
+    //number-like comparing
+    inline bool operator<(const Uncertainties &other)const{return val() < other.val();}
+    inline bool operator>(const Uncertainties &other)const{return val() > other.val();}
+    inline bool operator==(const Uncertainties&other)const{return val() == other.val();}
+    inline bool operator>=(const Uncertainties&other)const{return val() >= other.val();}
+    inline bool operator<=(const Uncertainties&other)const{return val() <= other.val();}
+    inline bool operator<(const NumberType&other)const{return val() < other;}
+    inline bool operator>(const NumberType&other)const{return val() > other;}
+    inline bool operator==(const NumberType&other)const{return val() == other;}
+    inline bool operator>=(const NumberType&other)const{return val() >= other;}
+    inline bool operator<=(const NumberType&other)const{return val() <= other;}
+    //arithmetic actions
+    Uncertainties &operator+=(const Uncertainties&other)
+    {
+        m_uncertainty = sqrt(pow(m_uncertainty, 2) + pow(other.m_uncertainty, 2));
+        m_value += other.m_value;
+        return *this;
+    }
+    Uncertainties&operator-=(const Uncertainties&other)
+    {
+        m_uncertainty = sqrt(pow(m_uncertainty, 2) + pow(other.m_uncertainty, 2));
+        m_value -= other.m_value;
+        return *this;
+    }
+    Uncertainties&operator*=(const Uncertainties&other)
+    {
+        m_uncertainty = sqrt(pow(m_uncertainty * other.val(), 2) + pow(other.m_uncertainty * val(), 2));
+        m_value *= other.m_value;
+        return *this;
+    }
+    Uncertainties&operator/=(const Uncertainties&other)
+    {
+        m_uncertainty = sqrt(pow(m_uncertainty / other.val(), 2)
+                     + pow(other.m_uncertainty * val() / pow(other.val(), 2), 2));
+        m_value /= other.m_value;
+        return *this;
+    }
+    //extending arithmetic actions
+    inline Uncertainties&operator+=(const NumberType&v){return operator+=(Uncertainties(v));}
+    inline Uncertainties&operator-=(const NumberType&v){return operator-=(Uncertainties(v));}
+    inline Uncertainties&operator*=(const NumberType&v){return operator*=(Uncertainties(v));}
+    inline Uncertainties&operator/=(const NumberType&v){return operator/=(Uncertainties(v));}
+    inline Uncertainties operator+(const Uncertainties&other)const{return Uncertainties(*this) += other;}
+    inline Uncertainties operator-(const Uncertainties&other)const{return Uncertainties(*this) -= other;}
+    inline Uncertainties operator*(const Uncertainties&other)const{return Uncertainties(*this) *= other;}
+    inline Uncertainties operator/(const Uncertainties&other)const{return Uncertainties(*this) /= other;}
+    inline Uncertainties operator+(const NumberType&other)const{return Uncertainties(*this) += other;}
+    inline Uncertainties operator-(const NumberType&other)const{return Uncertainties(*this) -= other;}
+    inline Uncertainties operator*(const NumberType&other)const{return Uncertainties(*this) *= other;}
+    inline Uncertainties operator/(const NumberType&other)const{return Uncertainties(*this) /= other;}
+    //IO
+    inline void output(std::ostream&str)const{
+	str<<m_value<<" "<<m_uncertainty;
+    }
+    inline void input(std::istream&str){
+	str>>m_value>>m_uncertainty;
+    }
+};
+template<size_t size,class numt>
+class Uncertainties
+{
+    template<size_t sizef, class n>friend class Uncertainties;
+public:
+    enum {UncertaintiesCount = size};
+    typedef numt NumberType;
+private:
+    Uncertainties<UncertaintiesCount-1,NumberType> m_lesser;
+    numt m_uncertainty;
+public:
+    virtual ~Uncertainties() {}
+    inline auto to_tuple()const->decltype(std::tuple_cat(m_lesser.to_tuple(), std::make_tuple(m_uncertainty)))
+    {
+        return std::tuple_cat(m_lesser.to_tuple(), std::make_tuple(m_uncertainty));
+    }
+    template<class... Args>
+    inline Uncertainties(const std::tuple<Args...> &v): m_lesser(v),m_uncertainty(std::get<UncertaintiesCount>(v)) {}
+    template<class Arg>
+    inline Uncertainties(const Arg&arg):m_lesser(arg),m_uncertainty(0){}
+    template<class Arg,class Arg2>
+    inline Uncertainties(const Arg&arg,const Arg2&arg2):m_lesser(arg),m_uncertainty(arg2){}
+    template<class numt2>
+    inline Uncertainties(const Uncertainties<UncertaintiesCount,numt2>&V):m_lesser(V.m_lesser),m_uncertainty(V.m_uncertainty){}
+#ifdef ____full_version_of_math_h_____
+    template<size_t index>
+    inline const NumberType&uncertainty()const
+    {
+	static_assert(index > 0,"uncertainty index is out of range");
+	static_assert(index<=UncertaintiesCount,"uncertainty index is out of range");
+        if constexpr(index == UncertaintiesCount)return m_uncertainty;
+	else return m_lesser.template uncertainty<index>();
+    }
+#else
+    template<size_t index>
+    const NumberType&uncertainty()const
+    {
+	static_assert(index > 0,"uncertainty index is out of range");
+	if(index>UncertaintiesCount)throw Exception<Uncertainties>("uncertainty index is out of range");
+        if(index == UncertaintiesCount)return m_uncertainty;
+	else return m_lesser.template uncertainty<index>();
+    }
+#endif
+    inline const NumberType&val()const{return m_lesser.val();}
+    //number-like comparing
+    inline bool operator<(const Uncertainties &other)const{return val() < other.val();}
+    inline bool operator>(const Uncertainties &other)const{return val() > other.val();}
+    inline bool operator==(const Uncertainties&other)const{return val() == other.val();}
+    inline bool operator>=(const Uncertainties&other)const{return val() >= other.val();}
+    inline bool operator<=(const Uncertainties&other)const{return val() <= other.val();}
+    inline bool operator<(const NumberType&other)const{return val() < other;}
+    inline bool operator>(const NumberType&other)const{return val() > other;}
+    inline bool operator==(const NumberType&other)const{return val() == other;}
+    inline bool operator>=(const NumberType&other)const{return val() >= other;}
+    inline bool operator<=(const NumberType&other)const{return val() <= other;}
+    //arithmetic actions
+    Uncertainties &operator+=(const Uncertainties&other)
+    {
+        m_uncertainty = sqrt(pow(m_uncertainty, 2) + pow(other.m_uncertainty, 2));
+        m_lesser += other.m_lesser;
+        return *this;
+    }
+    Uncertainties&operator-=(const Uncertainties&other)
+    {
+        m_uncertainty = sqrt(pow(m_uncertainty, 2) + pow(other.m_uncertainty, 2));
+        m_lesser -= other.m_lesser;
+        return *this;
+    }
+    Uncertainties&operator*=(const Uncertainties&other)
+    {
+        m_uncertainty = sqrt(pow(m_uncertainty * other.val(), 2) + pow(other.m_uncertainty * val(), 2));
+        m_lesser *= other.m_lesser;
+        return *this;
+    }
+    Uncertainties&operator/=(const Uncertainties&other)
+    {
+        m_uncertainty = sqrt(pow(m_uncertainty / other.val(), 2)
+                     + pow(other.m_uncertainty * val() / pow(other.val(), 2), 2));
+        m_lesser /= other.m_lesser;
+        return *this;
+    }
+    //extending arithmetic actions
+    inline Uncertainties&operator+=(const NumberType&v){return operator+=(Uncertainties(v));}
+    inline Uncertainties&operator-=(const NumberType&v){return operator-=(Uncertainties(v));}
+    inline Uncertainties&operator*=(const NumberType&v){return operator*=(Uncertainties(v));}
+    inline Uncertainties&operator/=(const NumberType&v){return operator/=(Uncertainties(v));}
+    inline Uncertainties operator+(const Uncertainties&other)const{return Uncertainties(*this) += other;}
+    inline Uncertainties operator-(const Uncertainties&other)const{return Uncertainties(*this) -= other;}
+    inline Uncertainties operator*(const Uncertainties&other)const{return Uncertainties(*this) *= other;}
+    inline Uncertainties operator/(const Uncertainties&other)const{return Uncertainties(*this) /= other;}
+    inline Uncertainties operator+(const NumberType&other)const{return Uncertainties(*this) += other;}
+    inline Uncertainties operator-(const NumberType&other)const{return Uncertainties(*this) -= other;}
+    inline Uncertainties operator*(const NumberType&other)const{return Uncertainties(*this) *= other;}
+    inline Uncertainties operator/(const NumberType&other)const{return Uncertainties(*this) /= other;}
+    //IO
+    inline void output(std::ostream&str)const{
+	m_lesser.output(str);
+	str<<" "<<m_uncertainty;
+    }
+    inline void input(std::istream&str){
+	m_lesser.input(str);
+	str>>m_uncertainty;
+    }
+
+};
+template<class numt, class... Args>
+inline Uncertainties < sizeof...(Args), numt > uncertainties(const numt &x, Args... args)
+{
+    return Uncertainties< sizeof...(Args), numt > (std::make_tuple(x, args...));
+}
+template<size_t index,size_t sz=index,class numt=double>
+inline Uncertainties<sz,numt> extend_value(const value<numt>&source)
+{
+    static_assert(sz>=index,"uncertainty indexing error");
+    return Uncertainties<sz,numt>(Uncertainties<index,numt>(source.val(),source.uncertainty()));
+}
+
+template<size_t i,class numt>
+inline std::ostream& operator<<(std::ostream&str,const Uncertainties<i,numt>&X)
+{
+    X.output(str);
+    return str;
+}
+template<size_t i,class numt>
+inline std::istream& operator>>(std::istream&str,Uncertainties<i,numt>&X)
+{
+    X.input(str);
+    return str;
+}
+
+
+};
+#endif
---
+++ tests/sigma3.cpp
@@ -0,0 +1,78 @@
+// this file is distributed under
+// LGPLv3 license
+#include <gtest/gtest.h>
+#include <math_h/sigma3.h>
+#include <math_h/randomfunc.h>
+using namespace std;
+using namespace MathTemplates;
+#define ALMOST_EQ(a,b) EXPECT_TRUE(abs(a-b)<0.0001)
+#define ALMOST_EQ2(a,b) EXPECT_TRUE(abs(a-b)<0.01)
+#define ALMOST_EQ3(a,b) EXPECT_TRUE(abs(a-b)<0.1)
+
+TEST(Uncertainties,base1)
+{
+    RandomUniform<> A(0.1,10);
+    RandomUniform<> a(0.1,1);
+    for (size_t i = 0; i < 50; i++) {
+	const double v=A(),u1=a(),u2=a();
+	const double w=A(),j1=a(),j2=a();
+	const value<> V1(v,u1),V2(v,u2);
+	const value<> W1(w,j1),W2(w,j2);
+	const auto M=uncertainties(v,u1,u2);
+	const auto N=uncertainties(w,j1,j2);
+
+	EXPECT_EQ((M+N).val(),(V1+W1).val());
+	EXPECT_EQ((M+N).val(),(V2+W2).val());
+	EXPECT_EQ((M+N).template uncertainty<1>(),(V1+W1).uncertainty());
+	EXPECT_EQ((M+N).template uncertainty<2>(),(V2+W2).uncertainty());
+
+	EXPECT_EQ((M-N).val(),(V1-W1).val());
+	EXPECT_EQ((M-N).val(),(V2-W2).val());
+	EXPECT_EQ((M-N).template uncertainty<1>(),(V1-W1).uncertainty());
+	EXPECT_EQ((M-N).template uncertainty<2>(),(V2-W2).uncertainty());
+
+	EXPECT_EQ((M*N).val(),(V1*W1).val());
+	EXPECT_EQ((M*N).val(),(V2*W2).val());
+	EXPECT_EQ((M*N).template uncertainty<1>(),(V1*W1).uncertainty());
+	EXPECT_EQ((M*N).template uncertainty<2>(),(V2*W2).uncertainty());
+
+	EXPECT_EQ((M/N).val(),(V1/W1).val());
+	EXPECT_EQ((M/N).val(),(V2/W2).val());
+	EXPECT_EQ((M/N).template uncertainty<1>(),(V1/W1).uncertainty());
+	EXPECT_EQ((M/N).template uncertainty<2>(),(V2/W2).uncertainty());
+    }
+}
+TEST(Uncertainties,base2)
+{
+    RandomUniform<> A(0.1,10);
+    RandomUniform<> a(0.1,1);
+    for (size_t i = 0; i < 50; i++) {
+	const value<> V(A(),a());
+	const auto M11=extend_value<1>(V);
+	EXPECT_EQ(M11.val(),V.val());
+	EXPECT_EQ(M11.template uncertainty<1>(),V.uncertainty());
+	const auto M12=extend_value<1,2>(V);
+	EXPECT_EQ(M12.val(),V.val());
+	EXPECT_EQ(M12.template uncertainty<1>(),V.uncertainty());
+	EXPECT_EQ(M12.template uncertainty<2>(),0);
+	const auto M13=extend_value<1,3>(V);
+	EXPECT_EQ(M13.val(),V.val());
+	EXPECT_EQ(M13.template uncertainty<1>(),V.uncertainty());
+	EXPECT_EQ(M13.template uncertainty<2>(),0);
+	EXPECT_EQ(M13.template uncertainty<3>(),0);
+	const auto M22=extend_value<2>(V);
+	EXPECT_EQ(M22.val(),V.val());
+	EXPECT_EQ(M22.template uncertainty<1>(),0);
+	EXPECT_EQ(M22.template uncertainty<2>(),V.uncertainty());
+	const auto M23=extend_value<2,3>(V);
+	EXPECT_EQ(M23.val(),V.val());
+	EXPECT_EQ(M23.template uncertainty<1>(),0);
+	EXPECT_EQ(M23.template uncertainty<2>(),V.uncertainty());
+	EXPECT_EQ(M23.template uncertainty<3>(),0);
+	const auto M33=extend_value<3>(V);
+	EXPECT_EQ(M33.val(),V.val());
+	EXPECT_EQ(M33.template uncertainty<1>(),0);
+	EXPECT_EQ(M33.template uncertainty<2>(),0);
+	EXPECT_EQ(M33.template uncertainty<3>(),V.uncertainty());
+    }
+}
