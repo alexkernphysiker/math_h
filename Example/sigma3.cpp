2eadd5683c08273878201d61660b1d4085c5e9ca
rundel <alexrndl@gmail.com>
2018-05-15 03:12:57 +0300

    example update

--- Example/sigma3.cpp
+++ Example/sigma3.cpp
@@ -21,8 +21,9 @@ int main()
     const auto theoretical_coefficient=extend_value<2,2>(value<>(3.5,0.5));//only systematical uncertainty
     //Let's save multiplied data with both uncertainties
     const auto result=data*theoretical_coefficient;
-    Plot("sigma3-example").Hist_2bars<1,2>(result,"","sigma3-output");
-    Plot("sigma3-example-2").Hist(wrap_hist(result));
+    Plot("sigma3-example").Hist_2bars<1,2>(result,"","sigma3-output");//draws two error bars
+    Plot("sigma3-example").Hist<1>(result,"","sigma3-output");//draws one error bar
+    Plot("sigma3-example-2").Hist(wrap_hist(result));//calculates one uncertainty taking statistical and systematical errors into account
     return 0;
 }
 
