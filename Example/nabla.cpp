58cb58bb84f8347883851338b91696a98157ef65
rundel <alexrndl@gmail.com>
2019-01-07 01:39:14 +0200

    example update

--- Example/nabla.cpp
+++ Example/nabla.cpp
@@ -8,11 +8,17 @@ using namespace GnuplotWrap;
 //This is an example how to use nabla operator
 int main()
 {
-    const auto Potential=[](const Vector<3>&r){return exp(-r.M_sqr()/8.0);};
+    const auto Potential=[](const Vector<3>&r){return 1.0/r.M_sqr();};
     Plot("nabla-example-potential")
-	.Line(SortedPoints<>([Potential](const double&c){return Potential(X()*c);},ChainWithStep(-10.,0.1,10.)));
+	.Line(SortedPoints<>([Potential](const double&c){
+	    return Potential(X()*c);
+	},ChainWithStep(-10.05,0.1,10.05)));
     Plot("nabla-example-field")
-	.Line(SortedPoints<>([Potential](const double&c){return (nabla(X()*c,0.01)*Potential).x();},ChainWithStep(-10.,0.1,10.)));
+	.Line(SortedPoints<>([Potential](const double&c){
+	    return (nabla(X()*c,0.01)*Potential).x();
+	},ChainWithStep(-10.05,0.1,10.05)));
     Plot("nabla-example-charge")
-	.Line(SortedPoints<>([Potential](const double&c){return nabla<3>(X()*c,0.01)*(nabla<3>(0.01)*Potential);},ChainWithStep(-10.,0.1,10.)));
+	.Line(SortedPoints<>([Potential](const double&c){
+	    return nabla<3>(X()*c,0.01)*(nabla<3>(0.01)*Potential);
+	},ChainWithStep(-10.05,0.1,10.05)));
 }
