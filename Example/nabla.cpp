90dc9c02ee32d7c6a300d6a0f01cf6a081248c2c
rundel <alexrndl@gmail.com>
2019-01-07 01:53:02 +0200

    example update

--- Example/nabla.cpp
+++ Example/nabla.cpp
@@ -12,13 +12,13 @@ int main()
     Plot("nabla-example-potential")
 	.Line(SortedPoints<>([Potential](const double&c){
 	    return Potential(X()*c);
-	},ChainWithStep(-10.05,0.1,10.05)));
+	},ChainWithStep(-10.1,0.2,10.1)));
     Plot("nabla-example-field")
 	.Line(SortedPoints<>([Potential](const double&c){
 	    return (nabla(X()*c,0.01)*Potential).x();
-	},ChainWithStep(-10.05,0.1,10.05)));
+	},ChainWithStep(-10.1,0.2,10.1)));
     Plot("nabla-example-charge")
 	.Line(SortedPoints<>([Potential](const double&c){
-	    return nabla<3>(X()*c,0.01)*(nabla<3>(0.01)*Potential);
-	},ChainWithStep(-10.05,0.1,10.05)));
+	    return (nabla<3>(0.01)*(nabla<3>(0.01)*Potential))(X()*c);
+	},ChainWithStep(-10.1,0.2,10.1)));
 }
