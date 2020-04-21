cdecba93a57be0b6ab84fea1200c91243cbcc519
Oleksandr Rundel <or@matterlabs.dev>
2020-04-21 23:51:43 +0300

    example update

--- Example/nabla.cpp
+++ Example/nabla.cpp
@@ -9,19 +9,10 @@ using namespace GnuplotWrap;
 int main()
 {
     const auto Potential=[](const Vector<3>&r){return 1.0/r.M_sqr();};
-    Plot("nabla-example-potential")
-	.Line(SortedPoints<>([Potential](const double&c){
-	    return Potential(X()*c);
-	},ChainWithStep(-10.1,0.2,10.1)));
-	Plot("nabla-example-field")
-	.Line(SortedPoints<>([Potential](const double&c){
-		auto field=nabla<3>(X()*c,0.01)*Potential;
-	    return field.x();
-	},ChainWithStep(-10.1,0.2,10.1)));
-    Plot("nabla-example-charge")
-	.Line(SortedPoints<>([Potential](const double&c){
-		auto field = nabla<3>(0.01)*Potential;
-		auto charge_density = nabla<3>(0.01)*field;
-	    return charge_density(X()*c);
-	},ChainWithStep(-10.1,0.2,10.1)));
+	auto field=nabla<3>(0.01)*Potential;
+	auto charge_density = nabla<3>(0.01)*field;
+
+    Plot("nabla-example-potential").Line(SortedPoints<>([Potential](const double&c){return Potential(X()*c);},ChainWithStep(-10.1,0.2,10.1)));
+	Plot("nabla-example-field").Line(SortedPoints<>([field](const double&c){return field.x(X()*c);},ChainWithStep(-10.1,0.2,10.1)));
+    Plot("nabla-example-charge").Line(SortedPoints<>([charge_density](const double&c){return charge_density(X()*c);},ChainWithStep(-10.1,0.2,10.1)));
 }
