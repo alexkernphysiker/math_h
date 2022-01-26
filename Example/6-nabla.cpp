4a498b7f01805949a4fa70bb4d1d09d4384d60bc
Oleksandr Rundel <alexrndl@gmail.com>
2022-01-26 21:17:58 +0000

    nabla example update

--- Example/6-nabla.cpp
+++ Example/6-nabla.cpp
@@ -1,6 +1,7 @@
 //This is an example how to use the math_h libary
 #include <iostream>
 #include <math_h/numeric_diff.h>
+#include <math_h/vectortransformations.h>
 #include <gnuplot_wrap.h>
 using namespace std;
 using namespace MathTemplates;
@@ -8,27 +9,28 @@ using namespace GnuplotWrap;
 //This is an example how to use nabla operator
 int main()
 {
-    const double delta = 0.001;
-    const auto Potential = [](const Vector<3>&r){ return 1.0 / r.length_sqr(); };
+    const double delta = 0.00001;
+    const auto Potential = [](const Vector<3>&r){ return 1.0 / r.length(); };
 	const auto field = -nabla<3>(delta) * Potential;
-	const auto charge_density = -nabla<3>(delta) * field;
+    const auto laplace = nabla<3>(delta)*nabla<3>(delta);
+	const auto charge_density = laplace * Potential;
 
     Plot("nabla-example-potential").Line(
         SortedPoints<>(
-            [&Potential](const double&c){return Potential(X()*c);},
-            ChainWithStep(0.1,0.02,2.1)
+            [&Potential](const double&c){return Potential(c*X()+0.5*Y());},
+            ChainWithStep(-2.1,0.02,2.1)
         )
     );
 	Plot("nabla-example-field").Line(
         SortedPoints<>(
-            [&field](const double&c){return field(c*X()).length();},
-            ChainWithStep(0.1,0.02,2.1)
+            [&field](const double&c){return field(c*X()+0.5*Y()).length();},
+            ChainWithStep(-2.1,0.02,2.1)
         )
     );
     Plot("nabla-example-charge").Line(
         SortedPoints<>(
-            [&charge_density](const double&c){return charge_density(c*X());},
-            ChainWithStep(0.1,0.02,2.1)
+            [&charge_density](const double&c){return charge_density(c*X()+0.5*Y());},
+            ChainWithStep(-2.1,0.02,2.1)
         )
     );
 }
